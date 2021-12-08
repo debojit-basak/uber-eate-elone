d5049125d7d964c6adfc4da291bb1ba9fb9e9c02
debojit-basak <39153154+debojit-basak@users.noreply.github.com>
2021-12-08 23:43:51 +0600

    facing problems with yelp

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
@@ -8,17 +8,46 @@ import SearchBar from '../components/SearchBar'
 const YELP_API_KEY = "fEGEXf5aRgEifD0ARaSjnoIG6R1ZXditU-EuUmjnI3u003SlT5oRIR5qvKOtQINM8Foxi7C_XXumrR4qYt-_Yg2Ye9T3gMvdxXiTbDFx9iQjjdl9pKJ660XmyeiwYXYx"
 
 export default function Home() {
-    const[restaurantData, setRestaurantData] = React.useState(localRestaurants)
-    const getRestaurantsFromYelp = () =>{
-        const yelpUrl = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiago";
+    const[restaurantData, setRestaurantData] = useState(localRestaurants)
+    // const getRestaurantsFromYelp = () =>{
+    //     const yelpUrl = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiago";
     
-    const apiOptions = {
-        headers: {
+    // const apiOptions = {
+    //     headers: {
+    //         Authorization: `Bearer ${YELP_API_KEY}`,
+    //     },
+    // };
+    //     return fetch(yelpUrl, apiOptions)
+    //     .then((res)=>res.json())
+    //     .then((json)=>setRestaurantData(json.businesses));
+    // };
+    // useEffect(() =>{
+    //     getRestaurantsFromYelp();
+    // }, []);
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiago`;
+    
+        const apiOptions = {
+          headers: {
             Authorization: `Bearer ${YELP_API_KEY}`,
-        },
-    };
-        return fetch(yelpUrl, apiOptions).then((res)=>res.json()).then((json)=>setRestaurantData(json.businesses));
-    };
+          },
+        };
+    
+        return fetch(yelpUrl, apiOptions)
+          .then((res) => res.json())
+          .then((json) =>
+            setRestaurantData(
+              json.businesses.filter((business) =>
+                business.transactions.includes(activeTab.toLowerCase())
+              )
+            )
+          );
+      };
+    
+      useEffect(() => {
+        getRestaurantsFromYelp();
+      }, []);
+    
     return (
         <SafeAreaView style={{backgroundColor: '#eee', flex:1}}>
             <View style={{backgroundColor: 'white', padding: 15}}>
